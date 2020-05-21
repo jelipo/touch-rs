f0a83b06ed513d009c61cd75bcca937577e6ce00
jelipo <me@jelipo.com>
2020-05-22 00:23:43 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::collections::HashMap;
+use std::env::var;
+use std::time::SystemTime;
+
+use crate::net::protocol::Socks5Protocal;
+use std::borrow::Borrow;
+
+mod net;
+
 fn main() {
+    let i = 1;
+    let map = HashMap::new();
+    let protocal = Socks5Protocal::new();
+
+    let string = Socks5Protocal::socks5_test();
     println!("Hello world!")
 }
---
+++ src/net/protocol.rs
@@ -0,0 +1,13 @@
+use std::collections::hash_map::RandomState;
+use std::collections::HashMap;
+
+pub struct Socks5Protocal {}
+
+impl Socks5Protocal {
+    //!
+    pub fn socks5_test() -> String {
+        return "hello".to_string();
+    }
+
+    pub fn new() -> Socks5Protocal {}
+}
