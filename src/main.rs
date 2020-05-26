fcd2c77315c1ad6b7738b9154294dc6344d09889
jelipo <me@jelipo.com>
2020-05-26 18:38:29 +0800

    demo

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,37 @@
-use tokio::net::TcpStream;
-use tokio::prelude::io::AsyncWriteExt;
+use async_std::io;
+use async_std::net::{TcpListener, TcpStream};
+use async_std::prelude::*;
+use async_std::task;
 
-mod net;
+async fn process(stream: TcpStream) -> io::Result<()> {
+    println!("Accepted from: {}", stream.peer_addr().unwrap());
 
-#[tokio::main]
-async fn main() {
-    let mut stream = TcpStream::connect("127.0.0.1:6142").await.unwrap();
-    println!("created stream");
+    let mut reader = stream.clone();
+    let mut writer = stream;
+    println!("{:?}", std::thread::current().id());
+    let mut arr = [0u8; 4];
+    while let read = reader.read(&mut arr).await {
+        let i = read.unwrap();
+        if i == 0 { break; }
+        println!("{:?}", arr);
+    }
+    println!("{:?}", "你好".as_bytes());
+    Ok(())
+}
 
-    let result = stream.write(b"hello world\n").await;
-    println!("wrote to stream; success={:?}", result.is_ok());
+fn main() -> io::Result<()> {
+    task::block_on(async {
+        let listener = TcpListener::bind("127.0.0.1:8081").await.unwrap();
+        println!("Listening on {}", listener.local_addr().unwrap());
+
+        let mut incoming = listener.incoming();
+
+        while let Some(stream) = incoming.next().await {
+            let stream = stream.unwrap();
+            task::spawn(async {
+                process(stream).await.unwrap();
+            });
+        }
+        Ok(())
+    })
 }
